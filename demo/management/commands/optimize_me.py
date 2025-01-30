49fcb2f3de44ac6aa0f6a403ee704c464a038f4d
Rye Welz Geselowitz <rye.welz.geselowitz@gmail.com>
2025-01-30 13:42:13 -0500

    update examples based on todays session!

--- demo/management/commands/optimize_me.py
+++ demo/management/commands/optimize_me.py
@@ -17,9 +17,9 @@ def get_formatted_author_intros(author_ids):
         )
     return intros
     
-def get_formatted_author_intros_OPTIMIZED(author_ids):
+def get_formatted_author_intros_OPTIMIZED_PREFETCH(author_ids):
     intros = []
-    authors = Author.objects.prefetch_related('books').filter(id__in=(author_ids))
+    authors = Author.objects.prefetch_related('books').filter(id__in=(author_ids)).order_by('id')
     for author in authors:
         intros.append(
             f'{author.name} is known for writing {len(author.books.all())} book(s).'
@@ -27,12 +27,21 @@ def get_formatted_author_intros_OPTIMIZED(author_ids):
     return intros
     
 
-def get_formatted_author_intros_OPTIMIZED_2(author_ids):
+def get_formatted_author_intros_OPTIMIZED_ANNOTATE(author_ids):
     intros = []
-    authors = Author.objects.filter(id__in=author_ids).annotate(book_count=Count("books"))
+    authors = Author.objects.filter(id__in=author_ids).annotate(book_count=Count("books")).order_by('id')
     for author in authors:
         intros.append(
             f'{author.name} is known for writing {author.book_count} book(s).'
+        )
+    return intros
+
+def get_formatted_author_intros_OPTIMIZED_ANNOTATE_AND_VALUES(author_ids):
+    intros = []
+    author_datas = Author.objects.values('name').filter(id__in=author_ids).annotate(book_count=Count("books")).order_by('id')
+    for author_data in author_datas:
+        intros.append(
+            f'{author_data['name']} is known for writing {author_data["book_count"]} book(s).'
         )
     return intros
 
@@ -60,18 +69,28 @@ def get_book_intros(book_ids):
         intros.append(f'{book.title} is by {author.name} and has {book.page_count} pages')
     return intros
 
-def get_book_intros_OPTIMIZED(book_ids):
+def get_book_intros_OPTIMIZED_PREFETCH_RELATED(book_ids):
     intros = []
     books = Book.objects.prefetch_related('author').filter(id__in=(book_ids))
     for book in  books:
         intros.append(f'{book.title} is by {book.author.name} and has {book.page_count} pages')
     return intros
 
-def get_book_intros_OPTIMIZED_2(book_ids):
+def get_book_intros_OPTIMIZED_ANNOTATE(book_ids):
     intros = []
     books = Book.objects.filter(id__in=(book_ids)).annotate(author_name=F("author__name")).order_by("id")
     for book in  books:
         intros.append(f'{book.title} is by {book.author_name} and has {book.page_count} pages')
+    return intros
+
+
+def get_book_intros_OPTIMIZED_SELECT_RELATED(book_ids):
+    # TODO: optimize (prefetch related or select related)
+    books = Book.objects.filter(id__in=book_ids).select_related('author')
+    intros = [
+        f'{book.title} is by {book.author.name} and has {book.page_count} pages'
+        for book in books
+    ]
     return intros
 
 class Command(BaseCommand):
@@ -81,32 +100,26 @@ class Command(BaseCommand):
         maybe_populate()
 
         author_ids = list(range(1, 50_000))
-        compare_runtimes_and_results(
-            get_formatted_author_intros,
-            get_formatted_author_intros_OPTIMIZED,
-            author_ids
-        )
-
-        compare_runtimes_and_results(
-            get_formatted_author_intros,
-            get_formatted_author_intros_OPTIMIZED_2,
-            author_ids
-        )
-
+        for optimized_func in [
+            get_formatted_author_intros_OPTIMIZED_PREFETCH,
+            get_formatted_author_intros_OPTIMIZED_ANNOTATE,
+            get_formatted_author_intros_OPTIMIZED_ANNOTATE_AND_VALUES
+        ]:
+            compare_runtimes_and_results(
+                get_formatted_author_intros,
+                optimized_func,
+                author_ids
+            )
+        
         compare_runtimes_and_results(
             get_highest_page_count_book_title,
             get_highest_page_count_book_title_OPTIMIZED,
         )
 
         book_ids = list(range(3, 51_000))
-        compare_runtimes_and_results(
-            get_book_intros,
-            get_book_intros_OPTIMIZED,
-            book_ids
-        )
-
-        compare_runtimes_and_results(
-            get_book_intros,
-            get_book_intros_OPTIMIZED_2,
-            book_ids
-        )
+        for optimized_func in [get_book_intros_OPTIMIZED_ANNOTATE, get_book_intros_OPTIMIZED_PREFETCH_RELATED, get_book_intros_OPTIMIZED_SELECT_RELATED]:
+            compare_runtimes_and_results(
+                get_book_intros,
+                optimized_func,
+                book_ids
+            )
